0e7636f68248cac4e0472abb48daa4cb1411c837
Daniel Izham <daniel.izham@gmail.com>
2020-04-08 11:25:53 +0300

    Add the basic structure of the project
    Most the classes and methods required in the project have
    been added in this commit. If any is missing, it can be added later
    on master which should then be merged into other branches to keep
    them up to date.

---
+++ src/datastructure/TreeTable.java
@@ -0,0 +1,41 @@
+package datastructure;
+
+import model.Student;
+
+public class TreeTable<E> {
+	public void insert(Student student) {
+
+	}
+
+	public Student search(int id) {
+		return null;
+	}
+
+	public void update(int id) {
+
+	}
+
+	public Student delete(int id) {
+		return null;
+	}
+
+	public void printStudent(int id) {
+
+	}
+
+	public void printTree(int year) {
+
+	}
+
+	public void printAll() {
+
+	}
+
+	public void showTree(int year) {
+
+	}
+
+	public void studentGPA(int gpa) {
+
+	}
+}
---
+++ src/model/Student.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Student {
+	private int id;
+	private String name;
+	private String address;
+	private double gpa;
+
+	public Student(int id, String name, String address, double gpa) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.gpa = gpa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+}
