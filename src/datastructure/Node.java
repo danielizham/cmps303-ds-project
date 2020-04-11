7162d8e9d4dced3231521641d02aabadf18af246
Ali Mohammadian <alim@Mac.local>
2020-04-11 18:20:58 +0300

    Add some stuff

--- src/datastructure/Node.java
+++ src/datastructure/Node.java
@@ -1,5 +1,20 @@
 package datastructure;
 
-public class Node<E> {
+import model.Student;
 
-}
+public class Node
+{
+	public int iData;              // data item (key)
+	public Student dData;           // data item
+	public Node leftChild;         // this node's left child
+	public Node rightChild;        // this node's right child
+
+	public void displayNode()      // display ourself
+	{
+		System.out.print('{');
+		System.out.print(iData);
+		System.out.print(", ");
+		System.out.print(dData);
+		System.out.print("} ");
+	}
+}  // end class Node
--- src/datastructure/TreeTable.java
+++ src/datastructure/TreeTable.java
@@ -2,17 +2,50 @@ package datastructure;
 
 import model.Student;
 
-public class TreeTable<E> {
-	public void insert(Student student) {
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
+public class TreeTable {
+
+	private Tree[] hashArray;
+
+	public TreeTable() {
+		hashArray = new Tree[20];
+		for (int i = 0; i < hashArray.length; i++)
+			hashArray[i] = new Tree();
+	}
+
+	public void insert(Student student) {
+		hashArray[h(student.getId())].insert(student.getId(),student);
 	}
 
 	public Student search(int id) {
-		return null;
+		Student student = hashArray[h(id)].find(id).dData;
+		if (student != null)
+			return student;
+		else
+			return null;
 	}
 
 	public void update(int id) {
+		Student student = search(id);
+		if (student != null) {
+			System.out.println("Current Student Information");
+			System.out.println(student);
 
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter new Name:");
+			String name = scanner.nextLine();
+			System.out.println("Enter new Address:");
+			String address = scanner.nextLine();
+			System.out.println("Enter new GPA:");
+			double gpa = scanner.nextDouble();
+
+			hashArray[h(id)].find(id).dData = new Student(id,name,address,gpa);
+
+		} else {
+			System.out.println("Student with such id does not exist!");
+		}
 	}
 
 	public Student delete(int id) {
@@ -20,22 +53,32 @@ public class TreeTable<E> {
 	}
 
 	public void printStudent(int id) {
-
+		System.out.println(search(id).toString());
 	}
 
 	public void printTree(int year) {
-
+		hashArray[year%20].printData();
 	}
 
 	public void printAll() {
-
+		for (Tree tree: hashArray )
+			tree.printData();
 	}
 
 	public void showTree(int year) {
-
+		hashArray[year%20].displayTree();
 	}
 
-	public void studentGPA(int gpa) {
-
+	public void studentGPA(double gpa) {
+		for (Tree tree: hashArray )
+			tree.printByGpa(gpa);
 	}
+
+	public int h(int id) {
+		String number = String.valueOf(id);
+		int year = Integer.parseInt(number.substring(0,4));
+		return year % 20;
+	}
+
+
 }
